0d19741bd5994b1434d15f46fd8f30c8f00a1381
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-05-02 16:24:23 +0100

    Orcus info added

---
+++ DwarfPlanets/orcus.py
@@ -0,0 +1,22 @@
+r_orcus = 620.0  # km, volumetric mean radius of orcus
+
+# Orbital Parameters
+a_orcus = 7190.7e6  # km
+p_orcus = 4530.0e6  # km
+semi_major_orcus = 5860.3e6 # km
+
+#mean_orbital_velocity_orcus = 4.53 # km/s
+#Max_orbital_velocity_orcus = 5.47 # km/s Will need to find, above is from wikipedia
+#Min_orbital_velocity_orcus = 5.37 # km/s Will need to find, above is from wikipedia
+
+i_orcus = 43.822 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_orcus = 0.2231336340984908 # eccentricity
+
+
+# orcus Mean Orbital Elements (J2000) - JPL Small object center
+semi_major_orcus_J2000 = 45.4494116430709 # AU
+e_orcus_J2000 = 0.2231336340984908 # eccentricity
+i_orcus_J2000 = 43.82162477388977 # degrees
+longitude_ascending_node_orcus_J2000 = 268.3860711048789 # degrees
+longitude_perihelion_orcus_J2000 = 73.80094017429205 # degrees, may need to check this one
+mean_anomaly_orcus_J2000 = 187.2113304099761 # degrees
