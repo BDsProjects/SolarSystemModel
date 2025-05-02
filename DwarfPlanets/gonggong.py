017912feec9635c2d6ddd0123622675c9e81e660
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-05-02 17:04:38 +0100

    gonggong base info
    gonggong base info

---
+++ DwarfPlanets/gonggong.py
@@ -0,0 +1,22 @@
+r_gonggong = 615.0  # km, volumetric mean radius of gonggong
+
+# Orbital Parameters
+a_gonggong = 15042.0e6  # km
+p_gonggong = 4987.5e6  # km
+semi_major_gonggong = 5860.3e6 # km
+
+#mean_orbital_velocity_gonggong = 4.53 # km/s
+#Max_orbital_velocity_gonggong = 5.47 # km/s Will need to find, above is from wikipedia
+#Min_orbital_velocity_gonggong = 5.37 # km/s Will need to find, above is from wikipedia
+
+i_gonggong = 30.82477466080239 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_gonggong = 0.5019847894173585 # eccentricity
+
+
+# gonggong Mean Orbital Elements (J2000) - JPL Small object center
+semi_major_gonggong_J2000 = 66.94496466784952 # AU
+e_gonggong_J2000 = 0.0366461532318176 # eccentricity
+i_gonggong_J2000 = 30.82477466080239 # degrees
+longitude_ascending_node_gonggong_J2000 = 336.8425914010824 # degrees
+longitude_perihelion_gonggong_J2000 = 206.7054165205142 # degrees, may need to check this one
+mean_anomaly_gonggong_J2000 = 111.0330292058526 # degrees
