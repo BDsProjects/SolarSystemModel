3b5d2ec1ff2c2321a4e65c6515d7b69a87698266
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-05-02 16:55:32 +0100

    quaoar base info

---
+++ DwarfPlanets/quaoar.py
@@ -0,0 +1,22 @@
+r_quaoar = 545.0  # km, volumetric mean radius of quaoar
+
+# Orbital Parameters
+a_quaoar = 6457.1e6  # km
+p_quaoar = 6220.5e6  # km
+semi_major_quaoar = 5860.3e6 # km
+
+#mean_orbital_velocity_quaoar = 4.53 # km/s
+#Max_orbital_velocity_quaoar = 5.47 # km/s Will need to find, above is from wikipedia
+#Min_orbital_velocity_quaoar = 5.37 # km/s Will need to find, above is from wikipedia
+
+i_quaoar = 7.991251547611665 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_quaoar = 0.0366461532318176 # eccentricity
+
+
+# quaoar Mean Orbital Elements (J2000) - JPL Small object center
+semi_major_quaoar_J2000 = 43.694 # AU
+e_quaoar_J2000 = 0.0366461532318176 # eccentricity
+i_quaoar_J2000 = 7.991251547611665 # degrees
+longitude_ascending_node_quaoar_J2000 = 189.0051966131428 # degrees
+longitude_perihelion_quaoar_J2000 = 163.7857886051256 # degrees, may need to check this one
+mean_anomaly_quaoar_J2000 = 290.9670782010869 # degrees
