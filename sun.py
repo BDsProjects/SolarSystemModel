b85874b57b1d54e13922825ec1e1c171cf666f96
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-05-06 21:24:46 +0100

    Create sun.py

---
+++ sun.py
@@ -0,0 +1,8 @@
+# https://nssdc.gsfc.nasa.gov/planetary/factsheet/sunfact.html
+sun_mean_radius = 695700 # km
+sun_earth_ratio = 109.2 # Earth radius to sun radius ratio
+sun_ellipticity = .00005
+
+# rotational and orbital params
+sun_sidereal_rotation_period = 609.12 # hours
+sun_obliquity_to_ecliptic = 7.25 # degrees
